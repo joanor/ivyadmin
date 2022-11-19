59978f48460e1f9282581c147059833c8483b97a
joanor <xixi090830@gmail.com>
2022-11-19 18:31:52 +0800

    test

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,9 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    BoxContainer: typeof import('./src/components/BoxContainer.vue')['default']
+    ButtonList: typeof import('./src/components/ButtonList.vue')['default']
+    Card: typeof import('./src/components/Card.vue')['default']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
@@ -14,7 +17,12 @@ declare module '@vue/runtime-core' {
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    Pagination: typeof import('./src/components/Pagination.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    SearchButton: typeof import('./src/components/SearchButton.vue')['default']
+    SearchContainer: typeof import('./src/components/SearchContainer.vue')['default']
+    SvgIcon: typeof import('./src/components/SvgIcon.vue')['default']
+    Table: typeof import('./src/components/Table.vue')['default']
   }
 }
